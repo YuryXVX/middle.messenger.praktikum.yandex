c01b78318271e6e1f2e2f289e195861efc7f2172
Багдасарян Юрий Арцвикович <yabagdas@mts.ru>
2022-12-18 21:02:27 +0300

    [spring_2] добавлен EventBus

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// import Router from './core/Router';
+// import { PAGES } from './pages';
+// import { registerComponent } from './utils/register-component';
+// import { ComponentsMap } from './components';
+
+import './styles/index.scss';
+
+// registerComponent(ComponentsMap);
+
+const root = document.querySelector('#root') as HTMLElement;
+
+
+console.log(root)
+// Router
+//   .instance(PAGES, root)
+//   .listen();
+
